54ec9b6473cb331bdbe83547ef19c9ed4f2109b5
Robin <robin1998@live.fr>
2018-11-27 14:27:49 +0100

    v_listerAuteur

--- controleurs/c_gererAuteurs.php
+++ controleurs/c_gererAuteurs.php
@@ -1,8 +1,61 @@
 <?php
+require_once 'modele/AuteurDal.class.php';
+require_once 'include/_reference.lib.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if(!isser($_REQUEST["action"])){
+    $action = "listerAuteurs";
+}
+else{
+    $action = $_REQUEST["action"];
+}
 
+$titrePage = 'Gestion des auteurs';
+
+$tabErreurs = array();
+$hasErrors = false;
+
+// variables pour la gestion des messages
+$msg=''; //msg passé à v_afficherMessages
+$lien=''; //msg passé à v_afficherErreurs
+
+
+// initialisation des variables
+$id = 0;
+$strNom = '';
+$strPrenom = '';
+$strAlias = '' ;
+$strNotes = '' ;
+
+switch($action){
+    case 'listerAuteurs' : {
+        $lesAuteurs = AuteurDal::loadGenres(1);
+        $nbAuteurs = count($lesAuteurs);
+        include("Vues/v_listerAuteurs.php");
+    }
+    break;
+
+    case 'consulterAuteur' : {
+
+    }
+    break;
+
+    case 'ajouterAuteur' : {
+
+    }
+    break;
+
+    case 'saisirAuteur' : {
+
+    }
+    break;
+
+    case 'modifierAuteur' : {
+
+    }
+    break;
+
+    case 'supprimerAuteur' : {
+
+    }
+    break;
+}
